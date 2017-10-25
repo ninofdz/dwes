7c50318cb3a26556d871c9cfd31f2f63badcbafe
ninofdz <tonino097@gmail.com>
2017-10-25 19:43:28 +0200

    25/10/17

--- ejercicios1/practica1/form.php
+++ ejercicios1/practica1/form.php
@@ -7,7 +7,7 @@
 
 <?php
 	//inicialitzar les variables
-	$user_nom  = $user_llin = $user_dni = $user_tlf = $user_email = $user_coneix = "";
+	$user_nom  = $user_llin = $user_dni =  $user_email = $user_tlf =  "";
 	$nomErr = $llinErr = $dniErr = $tlfErr = $coneixErr = "";
 	$emailErr = ".";
 	$comprobarEmail = "";
@@ -15,19 +15,17 @@
 
 
 
-	function clean_name($data) {
+	function test_input($data) {
   $data = trim($data);
   $data = stripslashes($data);
   $data = htmlspecialchars($data);
   return $data;
 }
 
-	function clean_dni($data) {
-	$data = trim($data);
-	$data = stripslashes($data);
-	$data = htmlspecialchars($data);
-	return $data;
-	}
+
+
+
+
 ?>
 
 <html>
@@ -48,14 +46,14 @@
 			echo "<style> #name{border:1px solid red} </style>";
 			echo "<style> .campo{display:block}</style>";
 		} else {
-			$user_nom = clean_name($_POST["user_nom"]);
+			$user_nom = test_input($_POST["user_nom"]);
 		}
 
 		if (empty($_POST["user_llin"])) {
 			$llinErr = "<span style='color:red'>*</span>";
 			echo "<style> .campo{display:block}</style>";
 		} else {
-			$user_llin = clean_name($_POST["user_llin"]);
+			$user_llin = test_input($_POST["user_llin"]);
 		}
 
 
@@ -68,11 +66,10 @@
 					$dniErr = "<span style='color:red'>*</span>";
 					echo "<style> .campo{display:block}</style>";
 			}
-			elseif (!is_numeric($dniNumeros) || is_numeric($dniLetra) || $dniNum !== 9  ) {
+			elseif ( !(ctype_alpha($dniLetra)) || !is_numeric($dniNumeros) || is_numeric($dniLetra) || $dniNum !== 9  ) {
 				$dniErr = "<span style='color:red'>Tiene que tener formato DNI</span>";
-				echo "<style> .campo{display:block}</style>";
 			} else {
-				$user_dni = clean_name($dni);
+				$user_dni = test_input($dni);
 			}
 
 		}
@@ -81,7 +78,7 @@
 			$tlfErr = "<span style='color:red'>*</span>";
 			echo "<style> .campo{display:block}</style>";
 		} else {
-			$user_tlf = clean_name($_POST["user_tlf"]);
+			$user_tlf = test_input($_POST["user_tlf"]);
 		}
 
 		$email = $_POST["user_email"];
@@ -96,7 +93,7 @@
 
 		} else {
 				$emailErr = "";
-				$user_email = clean_name($_POST["user_email"]);
+				$user_email = test_input($_POST["user_email"]);
 		}
 
 		if (isset ($_POST["coneixements"])) {
@@ -109,14 +106,38 @@
 		}
 
 
+
+
 }
 
 
 if( empty($nomErr) && empty($llinErr) && empty($dniErr) &&
 empty($tlfErr) && empty($emailErr)){
 
+	if (isset( $_POST["user_sexe"])){
+			$user_sexe = $_POST["user_sexe"];
+	}
 
-	?>
+	if (isset($_POST["user_dnaix"])){
+		$user_dnaix = $_POST["user_dnaix"];
+	}
+
+	if (isset( $_POST["user_exp"])){
+			$user_exp = $_POST["user_exp"];
+	}
+
+	if (isset ($_POST["user_coneixements"])) {
+		$user_coneix = $_POST['user_coneixements'];
+		$contar = count($user_coneix);
+		$numConeix = false;
+	}
+	if ($contar == 0) {
+			$numConeix = false;
+	} else {
+			$numConeix = true;
+	}
+
+?>
 
 
 	<table border="2" cellspacing="0" cellpadding="2" >
@@ -132,7 +153,7 @@ empty($tlfErr) && empty($emailErr)){
 				 <th>Sexe: </th>
 				 <td><?php echo $user_sexe ?></td>
 				 <th>Data de naixement: </th>
-				 <td><?php echo $data ?> </td>
+				 <td><?php echo $user_dnaix ?> </td>
 			 </tr>
 			 <tr>
 				 <th>Teléfon: </th>
@@ -146,14 +167,14 @@ empty($tlfErr) && empty($emailErr)){
 			 </tr>
 			 <tr>
 				 <th>Coneixements: </th>
-				 <td colspan="5"><?php if ($numSel == 0) {
-				 	echo "Sense coneixements";
-				}else {
-					foreach($_POST['coneixements'] as $coneixements){
-						echo  $coneixements . ", "; }
-				}
-
-				  ?></td>
+				 <td colspan="5">
+					 <?php
+					  if ($numConeix) {
+					 	foreach($_POST['user_coneixements'] as $value) { echo $value.' '; }
+						} else {
+						echo "No has seleccionado ninguna opción.";
+						} ?>
+					</td>
 			 </tr>
 		 </table>
 
@@ -174,19 +195,19 @@ empty($tlfErr) && empty($emailErr)){
 				<input type="text" id="name" name="user_nom"  value="<?php echo $user_nom?>">
 
 				<label for="llin">Llinatge:<?php echo $llinErr;?></label>
-				<input type="text"  id="llin" name="user_llin">
+				<input type="text"  id="llin" name="user_llin"  value="<?php echo $user_llin?>">
 
 				<label for="dni">DNI:<?php echo $dniErr ?></label>
-				<input type="text"  id="dni" name="user_dni">
+				<input type="text"  id="dni" name="user_dni" value="<?php echo $user_dni?>">
 
 				<label for="tlf">Telèfon:<?php echo $tlfErr;?></label>
-				<input type="text" id="tlf"  name="user_tlf">
+				<input type="text" id="tlf"  name="user_tlf" value="<?php echo $user_tlf?>">
 
 				<label for="email">Email:<?php echo $emailErr; ?> </label>
-				<input type="text" id="email"  name="user_email" placeholder="Ex: tuemail@dominio.com">
+				<input type="text" id="email"  name="user_email" placeholder="Ex: tuemail@dominio.com" value="<?php echo $user_email?>">
 
 				<label for="dnaix">Data naixement:</label>
-				<input type="date" id="dnaix" name="user_dnaix">
+				<input type="date" id="dnaix" name="user_dnaix" value="<?php echo $user_dnaix?>">
 
 				<label for="sexe">Sexe:</label>
   			<input type="radio" id="sexe" name="user_sexe" value="hombre"> Hombre
@@ -197,12 +218,12 @@ empty($tlfErr) && empty($emailErr)){
 			<fieldset>
 				<legend><span class="number">2</span>Tu perfil</legend>
 
-				<label id="xd" for="job">Experiencia laboral</label>
-					<select id="job" name="user_job">
-						<option value="sense">Sense experiència</option>
-						<option value="menor1"> < 1 any</option>
-						<option value="menor2"> < 2 anys</option>
-						<option value="mayor2"> > 2 anysAudi</option>
+				<label for="job">Experiencia laboral</label>
+					<select id="job" name="user_exp">
+						<option value="sense experiència">Sense experiència</option>
+						<option value="menor a 1 any"> < 1 any</option>
+						<option value="menor a 2 anys"> < 2 anys</option>
+						<option value="major a 2 anys"> > 2 anysAudi</option>
 					</select>
 
 				<label>Coneixements:</label>
